4a44eabeb2eb6beffe119bf6de07791c006a43f3
Anthony Rodríguez <anthonyrodriguez0506@outlook.com>
2026-04-09 16:24:08 -0400

    feat: add login method with BCrypt validation and JWT generation

--- src/main/java/com/anthony/auth/api/service/UserService.java
+++ src/main/java/com/anthony/auth/api/service/UserService.java
@@ -1,6 +1,7 @@
 package com.anthony.auth.api.service;
 import com.anthony.auth.api.model.User;
 import com.anthony.auth.api.repository.UserRepository;
+import com.anthony.auth.api.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,9 +12,22 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final JwtUtil jwtUtil;
 
     public User userRegister(User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
+
+    public String login(String email, String password) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+        if (!bCryptPasswordEncoder.matches(password, user.getPassword())) {
+            throw new RuntimeException("Contraseña incorrecta");
+        }
+
+        return jwtUtil.generateToken(email);
+    }
+
 }
